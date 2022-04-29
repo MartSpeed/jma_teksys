9efdfc2d8b6caa408af3ffc681d21bd2c049d51c
John Anthony <jmanthony.fullsail@gmail.com>
2022-04-29 00:13:09 -0500

    late night object practice and functionality

---
+++ anthony_udemy/src/anthony_udemy/Cars.java
@@ -0,0 +1,17 @@
+package anthony_udemy;
+
+public class Cars {
+	private int doors;
+	private int wheels;
+	private String model;
+	private String engine;
+	private String color;
+	
+	// getters/setters
+	public void setModel(String model) {
+		this.model = model;
+	}
+	public String getModel() {
+		return model;
+	}
+}
---
+++ anthony_udemy/src/anthony_udemy/Udemy_Main.java
@@ -0,0 +1,11 @@
+package anthony_udemy;
+
+public class Udemy_Main {
+
+	public static void main(String[] args) {
+		Cars car = new Cars();
+		car.setModel("Mustang");
+		System.out.println("the model is: " + car.getModel());
+	}
+
+}
