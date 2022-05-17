5760d1fd64f506e49d971b3dbfd0ec4de2b3ed7b
John Anthony <jmanthony.fullsail@gmail.com>
2022-05-17 08:49:52 -0500

    missed commits

--- SMS/src/main/java/jpa/service/StudentService.java
+++ SMS/src/main/java/jpa/service/StudentService.java
@@ -10,13 +10,14 @@ import javax.persistence.Query;
 import jpa.dao.StudentDAO;
 
 public abstract class StudentService implements StudentDAO {
+
 	EntityManagerFactory sessionFactory = Persistence.createEntityManagerFactory("SMSDB"); // hql entity manager?
 	EntityManager session = sessionFactory.createEntityManager(); // hql > sql method manager?
 
 	// This method reads the student table in your database and returns the data as
 	// a List<Student>
 	public List<StudentDAO> getAllStudents() {
-		Query query = session.createQuery("SELECT * FROM Student s");
+		Query query = session.createQuery("SELECT * FROM Student");
 		List<StudentDAO> studentList = query.getResultList();
 		return studentList;
 	}
@@ -25,8 +26,8 @@ public abstract class StudentService implements StudentDAO {
 	// for a Student with that email and returns a Student Object.
 	public StudentDAO getStudentByEmail(String sEmail) {
 		StudentDAO student = null;
-		Query query = session.createQuery("SELECT s FROM Student WHERE email = :email").setParameter("email", sEmail);
-		student = (StudentDAO)query.getSingleResult();
+		Query query = session.createQuery("SELECT * FROM Student WHERE email = :email").setParameter("email", sEmail);
+		student = (StudentDAO) query.getSingleResult();
 		return student;
 	}
 
