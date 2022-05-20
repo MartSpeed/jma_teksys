7b2268c4312fb9943a1c435cad5f62de66a9e0a7
John Anthony <jmanthony.fullsail@gmail.com>
2022-05-20 10:23:55 -0500

    FIX: didnt push the updates from previous work

--- anthony_udemy/src/speedConverter/Main.java
+++ anthony_udemy/src/speedConverter/Main.java
@@ -1,9 +1,6 @@
 package speedConverter;
 
 public class Main {
-
-<<<<<<< HEAD
-=======
 	public static void main(String[] args) {
 		//
 		long miles = SpeedConverter.toMilesPerHour(10.5);
@@ -12,6 +9,4 @@ public class Main {
 		//
 		SpeedConverter.printConversion(10.5);
 	}
-
->>>>>>> 147e31a7e5e0ac0b508a6c2600830d37db9c2c1f
 }
--- anthony_udemy/src/speedConverter/SpeedConverter.java
+++ anthony_udemy/src/speedConverter/SpeedConverter.java
@@ -1,14 +1,6 @@
 package speedConverter;
 
 public class SpeedConverter {
-<<<<<<< HEAD
-	//
-	public static long toMilesPerHour(double kilometerPerHour) {
-		// if less than 0, return -1;
-		if(kilometerPerHour < 0) {
-			return -1;
-		}
-=======
 	public static long toMilesPerHour(double kilometersPerHour) {
 		// return -1 if we get an invalid value
 		if(kilometersPerHour < 0) {
@@ -27,9 +19,8 @@ public class SpeedConverter {
 		}else {
 			long milesPerHour = toMilesPerHour(kilometersPerHour);
 			System.out.println(kilometersPerHour + 
-			"km/h = " + milesPerHour +
-			"mi/h");
+			" km/h = " + milesPerHour +
+			" mi/h");
 		}
->>>>>>> 147e31a7e5e0ac0b508a6c2600830d37db9c2c1f
 	}
 }
