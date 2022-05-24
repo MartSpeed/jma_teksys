202cf5ab3949a609f343a5163d5135b502209584
John Anthony <jmanthony.fullsail@gmail.com>
2022-05-24 08:19:44 -0500

    UPDATE: started the framework for a springboot template, will continue rest of instrucstions today, SBA and RESUME today

---
+++ ex_TeksysServlet/src/main/java/controller/FirstController.java
@@ -0,0 +1,14 @@
+package controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+// need to inform spring that that is a RESTcontroller
+@RestController
+public class FirstController {
+	// 
+	@GetMapping("/")
+	public String home() {
+		return "Hello from the controller";
+	}
+}
---
+++ ex_TeksysServlet/src/test/java/com/example/ex_TeksysServlet/ExTeksysServletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ex_TeksysServlet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExTeksysServletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
