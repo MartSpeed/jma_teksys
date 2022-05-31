86ad11b44ad32cd48e99d3e459503683cc27b420
John Anthony <jmanthony.fullsail@gmail.com>
2022-05-31 09:24:03 -0500

    initial commit: database driver setup complete using spring,

---
+++ ex_my_spring_data/src/main/java/com/example/ex_my_spring_data/ExMySpringDataApplication.java
@@ -0,0 +1,13 @@
+package com.example.ex_my_spring_data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExMySpringDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExMySpringDataApplication.class, args);
+	}
+
+}
