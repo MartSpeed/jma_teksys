a831273c8bf0be2a2d2933ce1dbbaef78707e370
John Anthony <jmanthony.fullsail@gmail.com>
2022-06-05 22:11:04 -0500

    initial commit for jma crud practice

---
+++ jma_springboot_backend/README.md
@@ -0,0 +1,9 @@
+###### Sunday June 5, 2020
+
+# Controller, Service and DAO Layer Practice
+
+- Springboot
+- Hibernate
+- Java
+- mySQL
+- Postman
---
+++ jma_springboot_backend/src/test/java/com/example/jma_springboot_backend/JmaSpringbootBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jma_springboot_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmaSpringbootBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
