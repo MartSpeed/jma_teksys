18410bd81d20dc38f440bcd3c02ba1aad4b0bdc4
John Anthony <jmanthony.fullsail@gmail.com>
2022-06-11 22:44:27 -0500

    UPDATE: elevator entity created

--- Anthony_John_Order_Parts_CaseStudy/src/main/java/com/example/Anthony_John_Order_Parts_CaseStudy/entity/Elevator.java
+++ Anthony_John_Order_Parts_CaseStudy/src/main/java/com/example/Anthony_John_Order_Parts_CaseStudy/entity/Elevator.java
@@ -1,9 +1,24 @@
 /*  AUTHOR: John Anthony
-    DATE:
-    DESCRIPTION:
+    DATE: 20220611
+    DESCRIPTION: the elevator hold the id number for each elevator object
 */
 
 package com.example.Anthony_John_Order_Parts_CaseStudy.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "elevator")
 public class Elevator {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int elevatorId; // PK for elevator
 }
