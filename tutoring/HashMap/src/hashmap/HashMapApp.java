43791fc2cd1a04ca4e6ead79708eeeb51d60e5dd
John Anthony <jmanthony.fullsail@gmail.com>
2022-06-13 17:38:01 -0500

    UPDATE: hashmap and arraylist reinforcement practices

---
+++ tutoring/HashMap/src/hashmap/HashMapApp.java
@@ -0,0 +1,12 @@
+package hashmap;
+
+public class HashMapApp {
+
+    public static void main(String[] args) {
+        // INSTANTIATE OBJECT
+        generateHashMap hashmap = new generateHashMap();
+
+        hashmap.generateHashMap();
+
+    }
+}
