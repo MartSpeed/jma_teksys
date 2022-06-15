04cf9b50e6b5d8a81f1d5111a0c326286b7aba80
John Anthony <jmanthony.fullsail@gmail.com>
2022-06-14 23:49:15 -0500

    WINUPDATE: added employee entity to database using springboot

--- jma_springboot_backend/src/main/java/com/example/jma_springboot_backend/model/Employee.java
+++ jma_springboot_backend/src/main/java/com/example/jma_springboot_backend/model/Employee.java
@@ -4,14 +4,24 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
+@Entity
+@Table(name="employees")
 public class Employee {
 
-    // adding a single comment
-    private long id;
+    // FIELDS
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id; // Employees Primary key
+
+    @Column(name="first_name", nullable = false)
     private String firstName;
+
+    @Column(name="last_name")
     private String lastName;
+
+    @Column(name="Email")
     private String email;
 }
