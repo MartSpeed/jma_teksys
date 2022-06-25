06704a37a9d1be860ea2e8dee5e35bea4a4a0350
John Anthony <jmanthony.fullsail@gmail.com>
2022-06-25 14:56:54 -0500

    FEAT: added employee button and button handler method to push the object to the thymeleaf DOM form

--- jma_thymetut/src/main/java/com/example/jma_thymetut/controller/EmployeeController.java
+++ jma_thymetut/src/main/java/com/example/jma_thymetut/controller/EmployeeController.java
@@ -15,6 +15,7 @@ public class EmployeeController {
     private EmployeeRepository repo;
 
     // GET
+    //HANDLER METHOD FOR FORM DISPLAY
     @GetMapping({"/showEmployees", "/", "/list"}) // what are the curly braces for?
     public ModelAndView showEmployees(){
         // create the ModelAndView OBJECT and takes the THYMELEAF template name "list-employees"
@@ -26,6 +27,18 @@ public class EmployeeController {
         return mav;
     }
 
+    // HANDLER METHOD FOR ADD EMPLOYEE BUTTON
+    @GetMapping("/addEmployeeForm")
+    public ModelAndView addEmployeeForm() {
+        // create the ModelAndView OBJECT and uses the THYMELEAF template name "add-employee-form"
+        ModelAndView mav = new ModelAndView("add-employee-form");
+        // new employee object to bind to the addEmployee form
+        EmployeeEntity newEmployee = new EmployeeEntity();
+        // add the newEmployee object to the ModelAndView Object, takes a K,V pair
+        mav.addObject("employee", newEmployee);
+        // return the object
+        return mav;
+    }
     // POST
     // UPDATE
     // DELETE
