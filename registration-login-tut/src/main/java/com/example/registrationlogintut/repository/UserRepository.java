ccf6a81370590866e2bdddd919fd6fe1c8a16008
John Anthony <jmanthony.fullsail@gmail.com>
2022-06-26 23:07:38 -0500

    FEAT: initial commit for the backend login implementation

--- registration-login-tut/src/main/java/com/example/registrationlogintut/repository/UserRepository.java
+++ registration-login-tut/src/main/java/com/example/registrationlogintut/repository/UserRepository.java
@@ -6,4 +6,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    // find a user by email address
+    User findByEmail(String email);
 }
