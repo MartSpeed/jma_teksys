ffe944d68ff3b76d22a1538a73fba48a6b332d56
John Anthony <jmanthony.fullsail@gmail.com>
2022-06-27 16:32:05 -0500

    FEAT: created the role entity class and setup values

--- Anthony_John_Order_Parts_CaseStudy/src/main/java/com/example/Anthony_John_Order_Parts_CaseStudy/entity/RoleEntity.java
+++ Anthony_John_Order_Parts_CaseStudy/src/main/java/com/example/Anthony_John_Order_Parts_CaseStudy/entity/RoleEntity.java
@@ -1,4 +1,27 @@
 package com.example.Anthony_John_Order_Parts_CaseStudy.entity;
 
+import lombok.*;
+
+import javax.persistence.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name="role")
 public class RoleEntity {
+    // FIELDS
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Long id;
+
+    @Column
+    private String name;
+    
+    public RoleEntity(String name) {
+        super();
+        this.name = name;
+    }
 }
