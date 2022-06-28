3f8e97bd40e055a052ce6fe274b3811adb5d39d2
John Anthony <jmanthony.fullsail@gmail.com>
2022-06-27 19:23:56 -0500

    FEAT: JUnit test function has been setup, implementation will be used when the parts database has been filled

---
+++ Anthony_John_Order_Parts_CaseStudy/src/test/java/com/example/Anthony_John_Order_Parts_CaseStudy/AircraftInventoryTest.java
@@ -0,0 +1,46 @@
+package com.example.Anthony_John_Order_Parts_CaseStudy;
+
+import com.example.Anthony_John_Order_Parts_CaseStudy.entity.AircraftPartEntity;
+import com.example.Anthony_John_Order_Parts_CaseStudy.entity.AircraftPartInventoryEntity;
+import com.example.Anthony_John_Order_Parts_CaseStudy.entity.UserEntity;
+import com.example.Anthony_John_Order_Parts_CaseStudy.repository.AircraftPartInventoryRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.annotation.Rollback;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Rollback(value = false)
+public class AircraftInventoryTest {
+
+    // DEPENDENCY INJECTION FOR PART ITEM
+    @Autowired
+    private AircraftPartInventoryRepository repo;
+
+    // USED TO PERFORM COMMON OPERATIONS ON THE DATABSE
+    @Autowired
+    private TestEntityManager testEntityManager;
+
+    // TEST AN ITEM ADDED TO THE CART WITH THE ID OF 7
+    @Test
+    public void testOnePart() {
+        //
+        AircraftPartEntity aircraftPart = testEntityManager.find(AircraftPartEntity.class, 7);
+        //
+        UserEntity userEntity = testEntityManager.find(UserEntity.class, 3);
+
+        // CREATE A NEW PART ITEM
+        AircraftPartInventoryEntity newPart = new AircraftPartInventoryEntity();
+        newPart.setUserEntity(userEntity);
+        newPart.setAircraftPartEntity(aircraftPart);
+        newPart.setQuantity(1);
+        AircraftPartInventoryEntity savedPart =  repo.save(newPart);
+        // SET ID PART HAS ID GREATER THAN 0
+        assertTrue(savedPart.getId() > 0);
+    }
+}
