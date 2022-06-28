e4b49a8a162a587aee27b5773614189fe93cc3a9
John Anthony <jmanthony.fullsail@gmail.com>
2022-06-28 10:28:04 -0500

    FEAT: quanity control functions and js implementation for button click events

---
+++ Anthony_John_Order_Parts_CaseStudy/src/main/resources/static/js/quantity-control.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    // click event of the minus button
+    @(".minusButton").on("click", function(event) {
+        event.preventDefault();
+        aircraftProductId = $(this).attr("pid");
+        quantityInput = $("#quantity" + aircraftProductId);
+
+        newQuantity = parseInt(quantityInput.val()) - 1;
+        if(newQuantity > 0) quantityInput.val(newQuantity);
+    });
+
+    // click event of the plus button
+})
