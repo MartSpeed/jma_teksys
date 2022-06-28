50f4c57656274d90c1ebfbc6020fd072f7d5af0c
John Anthony <jmanthony.fullsail@gmail.com>
2022-06-27 21:56:41 -0500

    FEAT: added the aircraft product model setup

--- Anthony_John_Order_Parts_CaseStudy/src/main/java/com/example/Anthony_John_Order_Parts_CaseStudy/entity/AircraftCartItems.java
+++ Anthony_John_Order_Parts_CaseStudy/src/main/java/com/example/Anthony_John_Order_Parts_CaseStudy/entity/AircraftCartItems.java
@@ -1,10 +1,42 @@
 package com.example.Anthony_John_Order_Parts_CaseStudy.entity;
 
-import javax.persistence.Column;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
+import javax.persistence.*;
+
+@Data
+@Getter
+@Setter
+@Entity
+@Table(name="cart_items")
 public class AircraftCartItems {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
 
-    // AIRCRAFT PART QUANTITY
-    @Column
+    // reference to aircraft product
+    // product can be added to cart items more than once
+    @ManyToOne
+    @JoinColumn(name="aircraft_product_id")
+    private AircraftProduct aircraftProduct;
+
+    //reference to the user
+    // user can add one more items in the aircraft cart
+    @ManyToOne
+    @JoinColumn(name="user_id")
+    private UserEntity userEntity;
+
+    // quantity of the product
     private int quantity;
+
+    //NO ARGS/ALL ARGS CONSTRUCT
+    public AircraftCartItems() {
+    }
+    public AircraftCartItems(AircraftProduct aircraftProduct, UserEntity userEntity, int quantity) {
+        this.aircraftProduct = aircraftProduct;
+        this.userEntity = userEntity;
+        this.quantity = quantity;
+    }
 }
