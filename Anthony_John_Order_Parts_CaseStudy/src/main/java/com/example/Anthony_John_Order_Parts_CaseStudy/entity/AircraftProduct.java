bf531a940888cb554fd2bcfa3ac18f1c9db34548
John Anthony <jmanthony.fullsail@gmail.com>
2022-06-28 09:13:12 -0500

    FEAT: creating cart html display

--- Anthony_John_Order_Parts_CaseStudy/src/main/java/com/example/Anthony_John_Order_Parts_CaseStudy/entity/AircraftProduct.java
+++ Anthony_John_Order_Parts_CaseStudy/src/main/java/com/example/Anthony_John_Order_Parts_CaseStudy/entity/AircraftProduct.java
@@ -1,4 +1,41 @@
 package com.example.Anthony_John_Order_Parts_CaseStudy.entity;
 
+import lombok.*;
+
+import javax.persistence.*;
+
+@Data
+@Getter
+@Setter
+@Entity
+@Table
 public class AircraftProduct {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    @Column
+    private String name;
+    @Column
+    private String mainImage;
+    @Column
+    private boolean inStock;
+    @Column
+    private float price;
+
+    // main image path
+    public String getMainImagePath(){
+        if(mainImage == null || id == null) return null;
+        return "/product-images/" + id + "/" + mainImage;
+    }
+
+    //NO ARGS/ALL ARGS
+    public AircraftProduct() {
+    }
+    public AircraftProduct(Integer id, String name, String mainImage, boolean inStock, float price) {
+        this.id = id;
+        this.name = name;
+        this.mainImage = mainImage;
+        this.inStock = inStock;
+        this.price = price;
+    }
 }
