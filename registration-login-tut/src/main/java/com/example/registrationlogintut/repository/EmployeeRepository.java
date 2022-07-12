df2286ebdabf914c8c321f7e23fa8dc59ce6853f
John Anthony <63987816+MartSpeed@users.noreply.github.com>
2022-07-12 10:17:38 -0500

    DELETE: cleaning files
    cleaning project folder

--- registration-login-tut/src/main/java/com/example/registrationlogintut/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.registrationlogintut.repository;
-
-import com.example.registrationlogintut.model.EmployeeEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmployeeRepository extends JpaRepository<EmployeeEntity, Long> {
-}
